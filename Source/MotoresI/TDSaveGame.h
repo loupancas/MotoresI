1ceb7bda067b2d3073c0fd9db7cc6271175ec523
loupancas <129621626+loupancas@users.noreply.github.com>
2025-06-26 18:04:46 -0500

    cambio

---
+++ Source/MotoresI/TDSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "TDSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOTORESI_API UTDSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
